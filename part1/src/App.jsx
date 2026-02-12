870209a0f7b5b7877920e70c43f8ea8dcf338bfc
Romeo-Martial <104857205+Romeo-Martial@users.noreply.github.com>
2026-02-13 00:31:20 +0100

    Submit exercise 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,13 +1,23 @@
 const Header = ({ course }) => <h1>{course}</h1>;
-const Content = ({ part, exercises }) => (
+const Part = ({ part, exercises }) => (
   <p>
     {part} {exercises}
   </p>
 );
-const Total = ({ exercices }) => (
+
+const Content = ({ data1, data2, data3 }) => {
+  return (
+    <div>
+      <Part part={data1.part} exercises={data1.exercises} />
+      <Part part={data2.part} exercises={data2.exercises} />
+      <Part part={data3.part} exercises={data3.exercises} />
+    </div>
+  );
+};
+const Total = ({ exercises }) => (
   <p>
     Number of exercises{" "}
-    {exercices.reduce((acc, elt) => {
+    {exercises.reduce((acc, elt) => {
       acc += elt;
       return acc;
     }, 0)}
@@ -26,10 +36,12 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total exercices={[exercises1, exercises2, exercises3]} />
+      <Content
+        data1={{ part: part1, exercises: exercises1 }}
+        data2={{ part: part2, exercises: exercises2 }}
+        data3={{ part: part3, exercises: exercises3 }}
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   );
 };
