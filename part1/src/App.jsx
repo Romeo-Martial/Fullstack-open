992df5501cc3dc1518a585d31608f4e05b802005
Romeo-Martial <104857205+Romeo-Martial@users.noreply.github.com>
2026-02-14 23:54:23 +0100

    Submit exercice 1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -26,24 +26,28 @@ const Total = ({ exercises }) => (
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content data1={part1} data2={part2} data3={part3} />
-      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Content data1={parts[0]} data2={parts[1]} data3={parts[2]} />
+      <Total
+        exercises={[parts[0].exercises, parts[1].exercises, parts[2].exercises]}
+      />
     </div>
   );
 };
